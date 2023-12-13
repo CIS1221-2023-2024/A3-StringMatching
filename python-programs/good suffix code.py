1b1cc7745cdd5fb8afe2b8671baabcd7aa82f71a
Montaser-Taher <montasertaher20@gmail.com>
2023-12-13 17:39:28 +0100

    Update good suffix code.py

--- python-programs/good suffix code.py
+++ python-programs/good suffix code.py
@@ -1,17 +1,74 @@
-def good_suffix_table(pattern):
-    pattern_length = len(pattern)
+public class Test {
 
-    suffix_table = [0] * pattern_length
-    last_prefix_position = pattern_length
+	static int TOTAL_CHARACTERS = 256;
 
-    for i in range(pattern_length - 1, -1, -1):
-        if is_prefix(pattern, i + 1):
-            last_prefix_position = i + 1
-        suffix_table[pattern_length - 1 - i] = last_prefix_position - i + pattern_length - 1
+	// The preprocessing function that fills the actual value of the last occurrence of a character
+	static void badCharacterHeuristic(char s[], int n,
+	int badCharacter[]) {
+		// First initializing all occurrences with -1
+		for (int i = 0; i < TOTAL_CHARACTERS; i++)
+			badCharacter[i] = -1;
 
-    for i in range(pattern_length - 1):
-        j = pattern_length - 1 - suffix_length(pattern, i)
-        if j < i:
-            suffix_table[j] = min(suffix_table[j], i - j + pattern_length - 1)
+		// Filling the actual value of the last occurrence of a character in the badCharacter array.
+		for (int i = 0; i < n; i++)
+			badCharacter[(int)s[i]] = i;
+	}
 
-    return suffix_table
+	/*
+	A function that searches the string and returns the occurrence.
+	*/
+	static void BoyerMooreAlgorithm(char text[], char pattern[]) {
+		// getting the size of the pattern and the string.
+		int m = pattern.length;
+		int n = text.length;
+
+		// defining a bacCharacter array.
+		int badCharacter[] = new int[TOTAL_CHARACTERS];
+
+		/*
+		Filling the badCharacter array with the current pattern using a helper function.
+		*/
+		badCharacterHeuristic(pattern, m, badCharacter);
+
+		// initially there is no shift
+		int shifts = 0;
+
+		// defining the boundaries of the searching
+		while (shifts <= (n - m)) {
+			int j = m - 1;
+
+			/*
+			If the pattern and text are matching, keep on reducing the j variable.
+			*/
+			while (j >= 0 && pattern[j] == text[shifts + j])
+				j--;
+
+			/*
+			If the pattern is present at current
+			shift, then j will become -1.
+			*/
+			if (j < 0) {
+				System.out.println("Pattern found at index: " + shifts);
+
+			/*
+			Shifting the pattern so that the next character of the text is aligned with its last occurrence in the pattern.
+			*/
+			shifts += (shifts + m < n) ? m - badCharacter[text[shifts + m]] : 1;
+			}
+
+			/*
+			It is shifting the pattern so that the bad character of the text is aligned with its last occurrence in the pattern. The max function helps to get the positive shift only.
+			*/
+			else
+				shifts += Math.max(1, j - badCharacter[text[shifts + j]]);
+		}
+	}
+
+	public static void main(String args[]) {
+
+		char text[] = "Scaler Topics".toCharArray();
+		char pattern[] = "Topics".toCharArray();
+
+		BoyerMooreAlgorithm(text, pattern);
+	}
+}
